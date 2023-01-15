381a2d894d5786a2fd931a682c2104e626342c83
FirstPeerapatSK <yggdrasillza@gmail.com>
2023-01-16 00:14:46 +0700

    Test Git2

--- app/src/main/java/com/example/alarmnote/FirstFragment.java
+++ app/src/main/java/com/example/alarmnote/FirstFragment.java
@@ -1,47 +1,38 @@
 package com.example.alarmnote;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.alarmnote.databinding.FragmentFirstBinding;
-
 public class FirstFragment extends Fragment {
 
-    private FragmentFirstBinding binding;
+
+    private Button button;
+
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-
-    }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        button = (Button) button.findViewById(R.id.button_selectalarm_button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                openSelectalarm(view);
             }
         });
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void openSelectalarm(View view){
+        Intent intent = new Intent(view.getContext(), Selectalarm.class);
+        startActivity(intent);
     }
 
 }
