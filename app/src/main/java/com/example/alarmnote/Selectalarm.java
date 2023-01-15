cf0d8a272e0ce27c848d302cfeb7d983cd9e06fe
FirstPeerapatSK <yggdrasillza@gmail.com>
2023-01-16 00:16:50 +0700

    Test Git2

---
+++ app/src/main/java/com/example/alarmnote/Selectalarm.java
@@ -0,0 +1,15 @@
+package com.example.alarmnote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Selectalarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selectalarm);
+
+    }
+}
